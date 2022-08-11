3c9b9d55282320cb91dc82bb278f45b261ec6e98
Valeri Devon <valeride@oa-n4.msk.21-school.ru>
2022-08-11 06:01:13 +0300

    fix: need make submitFunc

--- src/app.js
+++ src/app.js
@@ -1,5 +1,59 @@
-let container = document.querySelector('#popup')
-console.log(container)
+let formElement = document.querySelector('#popup')
+let profileContainer = document.querySelector('.profile__info')
+console.log(profileContainer)
+
+// открытие / закрыте попапа
+const popupCloseIcon = document.querySelectorAll('.popup__close');
+const popupEditIcon = document.querySelectorAll('.profile__edit-button');
+if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
+        const el = popupCloseIcon[index];
+        el.addEventListener('click', function (e) {
+            popupClose(el.closest('.popup'));
+            e.preventDefault();
+        })
+    }
+}
+
+if (popupEditIcon.length > 0) {
+    for (let index = 0; index < popupEditIcon.length; index++) {
+        const el = popupEditIcon[index];
+        el.addEventListener('click', function (e) {
+            popupOpen(formElement);
+            e.preventDefault();
+        })
+    }
+}
+
+function popupOpen(currentPopup) {
+    if (currentPopup) {
+        formElement.classList.remove('popup_closed');
+        formElement.classList.add('popup_opened');
+        unlock = true;
+    }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+    popupActive.classList.remove('open');
+    if (doUnlock) {
+        formElement.classList.toggle('popup_opened');
+    }
+}
+
+// заполнение из содержаниия страницы
+
+// let name = document.querySelector('.profile__username')
+// let status = document.querySelector('.profile__status')
+
+// console.log(name, status)
+
+// let formSearch = document.querySelector('.popup__form') // Находим форму в DOM
+// console.log(formSearch)
+
+
+
+// nameInput.innerHTML = name.innerHTML
+// console.log(nameInput)
 
 
 
@@ -8,11 +62,17 @@ console.log(container)
 
 
 
-// Находим форму в DOM
-let formElement = document.querySelector
-// Находим поля формы в DOM
-let nameInput = // Воспользуйтесь инструментом .querySelector()
-// let jobInput = // Воспользуйтесь инструментом .querySelector()
+
+let formSearch = document.querySelector('.popup__form') // Находим форму в DOM
+console.log(formSearch)
+let nameInput = formSearch.querySelector('.popup__input-field_value_name') // Находим поля формы в DOM
+console.log(nameInput)
+let jobInput = formSearch.querySelector('.popup__input-field_value_job')
+console.log(jobInput)
+
+
+
+
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -20,8 +80,11 @@ function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
+    nameInput.textContent = nameInput.value
+    console.log(nameInput)  
+    // jobInput.value = 
+    
+                                           // Получите значение полей jobInput и nameInput из свойства value
 
     // Выберите элементы, куда должны быть вставлены значения полей
 
