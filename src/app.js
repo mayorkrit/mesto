53f4c572e390ceea2faec90a37575bafbb9f8545
Igor Loginov <igorloginovofficial@gmail.com>
2022-08-12 07:21:47 +0300

    huynya

--- src/app.js
+++ src/app.js
@@ -1,18 +1,63 @@
-let container = document.querySelector('#popup')
-console.log(container)
+let formElement = document.querySelector('#popup')
+console.log(formElement)
+
+// открытие/ закрыте попапа
+const popupCloseIcon = document.querySelectorAll('.popup__close');
+const popupEditIcon = document.querySelectorAll('.profile__edit-button');
+if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
+        const el = popupCloseIcon[index];
+        el.addEventListener('click', function (e) {
+            popupClose(el.closest('.popup'));
+            e.preventDefault();
+        })
+    }
+}
+
+if (popupEditIcon.length > 0) {
+    for (let index = 0; index < popupEditIcon.length; index++) {
+        const el = popupEditIcon[index];
+        el.addEventListener('click', function (e) {
+            popupOpen(formElement);
+            e.preventDefault();
+        })
+    }
+}
+
+function popupOpen(currentPopup) {
+    if (currentPopup) {
+        formElement.classList.remove('popup_closed');
+        formElement.classList.add('popup_opened');
+        unlock = true;
+    }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+    popupActive.classList.remove('open');
+    if (doUnlock) {
+        formElement.classList.toggle('popup_opened');
+    }
+}
+
+// заполнение из содержаниия страницы
+let nameInputValue = document.querySelector('.profile__username')
+let jobInputValue = document.querySelector('.profile__status')
+console.log(jobInputValue)
 
 
 
 
+// let formSearch = document.querySelector('.popup__form') // Находим форму в DOM
+// console.log(formSearch)
+
+// let nameInput = formSearch.querySelector('.popup__input-field_value_name') // Находим поля формы в DOM
+// console.log(nameInput)
+
+// let jobInput = formSearch.querySelector('.popup__input-field_value_job')
+// console.log(jobInput)
 
 
 
-
-// Находим форму в DOM
-let formElement = document.querySelector
-// Находим поля формы в DOM
-let nameInput = // Воспользуйтесь инструментом .querySelector()
-// let jobInput = // Воспользуйтесь инструментом .querySelector()
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -20,7 +65,6 @@ function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
-
     // Получите значение полей jobInput и nameInput из свойства value
 
     // Выберите элементы, куда должны быть вставлены значения полей
